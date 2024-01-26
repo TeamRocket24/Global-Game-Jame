78c332e7f7a39a4476cfc655285e73c95b530fd1
Alejandro Figueroa <alejandro.figueroa.rodriguez.01@gmail.com>
2024-01-26 14:53:59 -0600

    Se quito la magia y se crearon los npcs con sus respectivos dialogos

--- code/level.py
+++ code/level.py
@@ -9,8 +9,9 @@ from weapon import Weapon
 from ui import UI
 from enemy import Enemy
 from particles import AnimationPlayer
-from magic import MagicPlayer
 from upgrade import Upgrade
+from npc import NPC
+from settings import npc_data
 
 class Level:
 	def __init__(self):
@@ -22,6 +23,7 @@ class Level:
 		# sprite group setup
 		self.visible_sprites = YSortCameraGroup()
 		self.obstacle_sprites = pygame.sprite.Group()
+		self.npc_sprites = pygame.sprite.Group()
 
 		# attack sprites
 		self.current_attack = None
@@ -37,7 +39,6 @@ class Level:
 
 		# particles
 		self.animation_player = AnimationPlayer()
-		self.magic_player = MagicPlayer(self.animation_player)
 
 	def create_map(self):
 		layouts = {
@@ -51,6 +52,7 @@ class Level:
 			'objects': import_folder('../graphics/objects')
 		}
 
+
 		for style,layout in layouts.items():
 			for row_index,row in enumerate(layout):
 				for col_index, col in enumerate(row):
@@ -63,28 +65,54 @@ class Level:
 							random_grass_image = choice(graphics['grass'])
 							Tile(
 								(x,y),
-								[self.visible_sprites,self.obstacle_sprites,self.attackable_sprites],
+								[
+									self.visible_sprites,
+									self.obstacle_sprites,
+									self.attackable_sprites
+								],
 								'grass',
 								random_grass_image)
 
 						if style == 'object':
 							surf = graphics['objects'][int(col)]
-							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
+							Tile(
+								(x,y),
+								[
+									self.visible_sprites,
+									self.obstacle_sprites
+								],
+								'object',
+								surf
+							)
 
 						if style == 'entities':
 							if col == '394':
+								cont = 0
+								for name in npc_data.keys():
+									cont += 50
+
+									NPC(
+										name,
+										(x + cont, y + cont),
+										npc_data[name]["graphic"],
+										[self.visible_sprites, self.npc_sprites],
+										npc_data[name]["dialogue"]
+									)
+
 								self.player = Player(
 									(x,y),
 									[self.visible_sprites],
 									self.obstacle_sprites,
 									self.create_attack,
 									self.destroy_attack,
-									self.create_magic)
+								)
+
 							else:
 								if col == '390': monster_name = 'bamboo'
 								elif col == '391': monster_name = 'spirit'
 								elif col == '392': monster_name ='raccoon'
 								else: monster_name = 'squid'
+								
 								Enemy(
 									monster_name,
 									(x,y),
@@ -92,49 +120,67 @@ class Level:
 									self.obstacle_sprites,
 									self.damage_player,
 									self.trigger_death_particles,
-									self.add_exp)
+									self.add_exp
+								)
 
 	def create_attack(self):
 		
-		self.current_attack = Weapon(self.player,[self.visible_sprites,self.attack_sprites])
-
-	def create_magic(self,style,strength,cost):
-		if style == 'heal':
-			self.magic_player.heal(self.player,strength,cost,[self.visible_sprites])
-
-		if style == 'flame':
-			self.magic_player.flame(self.player,cost,[self.visible_sprites,self.attack_sprites])
+		self.current_attack = Weapon(
+			self.player,
+			[
+				self.visible_sprites,
+				self.attack_sprites
+			]
+		)
 
 	def destroy_attack(self):
-		if self.current_attack:
+		if self.current_attack: 
 			self.current_attack.kill()
 		self.current_attack = None
 
 	def player_attack_logic(self):
 		if self.attack_sprites:
 			for attack_sprite in self.attack_sprites:
-				collision_sprites = pygame.sprite.spritecollide(attack_sprite,self.attackable_sprites,False)
+				collision_sprites = pygame.sprite.spritecollide(
+					attack_sprite,
+					self.attackable_sprites,
+					False
+				)
 				if collision_sprites:
 					for target_sprite in collision_sprites:
 						if target_sprite.sprite_type == 'grass':
 							pos = target_sprite.rect.center
 							offset = pygame.math.Vector2(0,75)
 							for leaf in range(randint(3,6)):
-								self.animation_player.create_grass_particles(pos - offset,[self.visible_sprites])
+								self.animation_player.create_grass_particles(
+									pos - offset,
+									[self.visible_sprites]
+								)
 							target_sprite.kill()
 						else:
-							target_sprite.get_damage(self.player,attack_sprite.sprite_type)
+							target_sprite.get_damage(
+								self.player,
+								attack_sprite.sprite_type
+							)
 
 	def damage_player(self,amount,attack_type):
 		if self.player.vulnerable:
 			self.player.health -= amount
 			self.player.vulnerable = False
 			self.player.hurt_time = pygame.time.get_ticks()
-			self.animation_player.create_particles(attack_type,self.player.rect.center,[self.visible_sprites])
+			self.animation_player.create_particles(
+				attack_type,
+				self.player.rect.center,
+				[self.visible_sprites]
+			)
 
 	def trigger_death_particles(self,pos,particle_type):
 
-		self.animation_player.create_particles(particle_type,pos,self.visible_sprites)
+		self.animation_player.create_particles(
+			particle_type,
+			pos,
+			self.visible_sprites
+		)
 
 	def add_exp(self,amount):
 
@@ -146,13 +192,14 @@ class Level:
 
 	def run(self):
 		self.visible_sprites.custom_draw(self.player)
-		self.ui.display(self.player)
+		self.ui.display(self.player, self.npc_sprites)
 		
 		if self.game_paused:
 			self.upgrade.display()
 		else:
 			self.visible_sprites.update()
 			self.visible_sprites.enemy_update(self.player)
+			self.visible_sprites.npc_update(self.player)
 			self.player_attack_logic()
 		
 
@@ -186,6 +233,19 @@ class YSortCameraGroup(pygame.sprite.Group):
 			self.display_surface.blit(sprite.image,offset_pos)
 
 	def enemy_update(self,player):
-		enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite,'sprite_type') and sprite.sprite_type == 'enemy']
+		enemy_sprites = [
+			sprite 
+			for sprite in self.sprites() 
+			if hasattr(sprite,'sprite_type') and sprite.sprite_type == 'enemy'
+		]
 		for enemy in enemy_sprites:
 			enemy.enemy_update(player)
+
+	def npc_update(self, player):
+		npc_sprites = [
+			sprite 
+			for sprite in self.sprites() 
+			if hasattr(sprite,'sprite_type') and sprite.sprite_type == 'npc'
+		]
+		for npc in npc_sprites:
+			npc.npc_update(player)
--- code/ui.py
+++ code/ui.py
@@ -1,9 +1,12 @@
 import pygame
+import settings
 from settings import * 
+import pygame.camera
 
 class UI:
 	def __init__(self):
 		
+		self.cont = 0
 		# general 
 		self.display_surface = pygame.display.get_surface()
 		self.font = pygame.font.Font(UI_FONT,UI_FONT_SIZE)
@@ -18,12 +21,6 @@ class UI:
 			path = weapon['graphic']
 			weapon = pygame.image.load(path).convert_alpha()
 			self.weapon_graphics.append(weapon)
-
-		# convert magic dictionary
-		self.magic_graphics = []
-		for magic in magic_data.values():
-			magic = pygame.image.load(magic['graphic']).convert_alpha()
-			self.magic_graphics.append(magic)
 
 
 	def show_bar(self,current,max_amount,bg_rect,color):
@@ -50,6 +47,48 @@ class UI:
 		self.display_surface.blit(text_surf,text_rect)
 		pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,text_rect.inflate(20,20),3)
 
+	def show_npc_label(self, npc_name, *pos):
+		font = pygame.font.Font(None, 50)
+		text_surface = font.render(npc_name, True, (255, 255, 255))
+		rect = text_surface.get_rect()
+		label_height = text_surface.get_height()
+		self.display_surface.blit(
+			text_surface, 
+			pos
+		)
+
+
+	def show_npc_dialogue(self,npc):
+		w = 120
+		h = 630
+		text_font = pygame.font.Font(None, 30)
+		text_dialogue = npc.get_dialogue()
+		text_displayed = ""
+
+		for j in text_dialogue.split(" "):
+			text = text_font.render(j+" ", True, [0, 0, 0])
+			if w+text.get_width()>1200:
+				h+=text.get_height()
+				w = 120
+			textRect = text.get_rect(topleft = [w, h])
+			w+=text.get_width()
+			self.display_surface.blit(text, textRect)
+
+		button_font = pygame.font.Font(None, 35)
+		if npc.has_more_dialogue():
+			button_surface = button_font.render("siguiente", True, (255, 0, 0))
+			self.display_surface.blit(
+				button_surface, 
+				(1000, 600)
+			)
+		else:
+			button_surface = button_font.render("Dejar de Hablar", True, (255, 0, 0))
+			self.display_surface.blit(
+				button_surface, 
+				(1000, 600)
+			)
+
+
 	def selection_box(self,left,top, has_switched):
 		bg_rect = pygame.Rect(left,top,ITEM_BOX_SIZE,ITEM_BOX_SIZE)
 		pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
@@ -66,18 +105,14 @@ class UI:
 
 		self.display_surface.blit(weapon_surf,weapon_rect)
 
-	def magic_overlay(self,magic_index,has_switched):
-		bg_rect = self.selection_box(80,635,has_switched)
-		magic_surf = self.magic_graphics[magic_index]
-		magic_rect = magic_surf.get_rect(center = bg_rect.center)
-
-		self.display_surface.blit(magic_surf,magic_rect)
-
-	def display(self,player):
+	def display(self,player, npc_sprites):
 		self.show_bar(player.health,player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
 		self.show_bar(player.energy,player.stats['energy'],self.energy_bar_rect,ENERGY_COLOR)
 
 		self.show_exp(player.exp)
 
 		self.weapon_overlay(player.weapon_index,not player.can_switch_weapon)
-		self.magic_overlay(player.magic_index,not player.can_switch_magic)
+
+		if player.npc_dialoguing:
+			npc = player.npc_dialoguing
+			self.show_npc_dialogue(npc)
